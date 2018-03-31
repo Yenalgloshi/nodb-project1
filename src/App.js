e07a50107038a87b372040626867eca9d61cb1b4
Yenalgloshi <jasonbegay@msn.com>
2018-03-31 14:57:10 -0600

    pulldown menus added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      someArray: [],
-      apiUrl:'https://bgg-json.azurewebsites.net/collection/edwalter'
+      filterValues: [],
+      gamesToDisplay: []
     }
   }
 
@@ -19,16 +19,70 @@ componentDidMount(){
   })
 }
 
-   
+getMyGameList(){
+  axios.get('/api/games')
+    promise.then( res => {
+      this.setState({
+        gamesToDisplay: res.data
+      })
+    })
+}   
     
+saveGame(){
+
+}
+
+deleteGame(){
+
+}
+
+
 
 render() {
     return (
       <div className="App">
-        
+        <div className=''>
+          <div className='btn-dropDown'>
+            <button className='get-game-list btn' 
+                    onClick={ this.getMyGameList }>
+              Get A List Of Games
+            </button>
+
+            <select onChange={ this.filterByPlayTime }
+                    ref='selectedPlayTime'
+                    className='btn-sp'
+                    value="">
+              <option value="" disabled>Filter by play time</option>
+              <option value="15min">Less than 15 min</option>
+              <option value="30min">Less than 30 min</option>
+              <option value="45min">Less than 45 min</option>
+              <option value="60min">Less than 60 min</option>
+              <option value="90min">Less than 90 min</option>
+            </select>
+
+            <select ref='selected#OfPlayers'
+                    onChange={ this.filterByNumPlayers }
+                    className='btn-sp'
+                    value="">
+              <option value="" disabled>How many players?</option>
+              <option value="one">1</option>
+              <option value="two">2</option>
+              <option value="three">3</option>
+              <option value="four">4</option>
+              <option value="five">5</option>
+              <option value="six">6</option>
+            </select>
+
+            <button className='go-btn btn'
+                    onClick={ this.getMyGameList }>
+              Find Me Some Games To Play!
+            </button>
+          </div> 
+        </div>
       </div>
     );
-  }
+  }    
 }
+
 
 export default App;
